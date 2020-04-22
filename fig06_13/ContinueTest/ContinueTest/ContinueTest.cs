71b0e3a24ab0215d9aa66f043193219fb59aa8ec
Roberto Kai Hsi Hoo <prof.robertohoo@gmail.com>
2020-04-22 19:49:43 -0300

    ok

--- fig06_13/ContinueTest/ContinueTest/ContinueTest.cs
+++ fig06_13/ContinueTest/ContinueTest/ContinueTest.cs
@@ -4,19 +4,31 @@ using System;
 
 public class ContinueTest
 {
-   public static void Main( string[] args )
+   public static void Main(string[] args)
    {
-      for ( int count = 1; count <= 10; ++count ) // loop 10 times
+      for (int count = 1; count <= 10; ++count) // loop 10 times
       {
-         if ( count == 5 ) // if count is 5, 
+         if (count == 5) // if count is 5, 
             continue; // skip remaining code in loop
 
-         Console.Write( "{0} ", count );
+         Console.Write("{0} ", count);
       } // end for
 
-      Console.WriteLine( "\nUsed continue to skip displaying 5" );
-      
-       // Keep the console open in debug mode.      
+      Console.WriteLine("\nUsed continue to skip displaying 5\n");
+
+      /* In this example, a counter is initialized to count from 1 to 10. By using the continue 
+       * statement in conjunction with the expression (i < 9), the statements between continue 
+       * and the end of the for body are skipped. */
+      for (int i = 1; i <= 10; i++)
+      {
+         if (i < 9)
+         {
+            continue;
+         }
+         Console.WriteLine(i);
+      }
+
+      // Keep the console open in debug mode.      
       Console.WriteLine("\nPress any key to exit.");
       Console.ReadKey();
 
